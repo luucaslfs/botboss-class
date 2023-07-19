3907a33391e658bfc08a327e756b90bf0880430c
PET Curso <petcurso@WINDOWS.CIN.UFPE.BR>
2023-07-19 14:47:26 -0300

    de quem é esse git ?

---
+++ twitter-demo/happy-turtle/index.js
@@ -0,0 +1,14 @@
+import { main } from "./turtles.js"
+
+import { writeFileSync } from 'fs'
+
+(async () => {
+  const turtles = await main()
+
+  writeFileSync('./output.json', JSON.stringify(turtles))
+  process.exit(0)
+})()
+
+
+
+
